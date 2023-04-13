163d8f18afb15bf3a4c57c4e30ed6f67d48ca5cd
Bladimir Minga <bsminga@pichincha.com>
2023-04-13 17:51:43 -0500

    Se agregan las dependencias básicas

---
+++ src/main/java/com/bp/ensayo/ct1/Ct1Application.java
@@ -0,0 +1,13 @@
+package com.bp.ensayo.ct1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bp/ensayo/ct1/Ct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bp.ensayo.ct1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
