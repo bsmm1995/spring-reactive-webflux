22868b865fcd77f360f28d3d9debd5f08e27690b
Bladimir Minga <bsminga@pichincha.com>
2023-04-13 18:16:54 -0500

    Se agrega version basica de arquetipo

---
+++ src/main/java/com/bp/ensayo/ct1/repository/TestRepository.java
@@ -0,0 +1,8 @@
+package com.bp.ensayo.ct1.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends CrudRepository {
+}
