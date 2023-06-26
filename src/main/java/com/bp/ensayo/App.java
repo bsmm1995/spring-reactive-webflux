eaf288229b7672a7845b6994dffc8b8f8ad6f08c
Bladimir Minga <bsminga@pichincha.com>
2023-06-25 22:12:14 -0500

    Se actualiza version de spring boot

--- src/main/java/com/bp/ensayo/App.java
+++ src/main/java/com/bp/ensayo/App.java
@@ -3,11 +3,17 @@ package com.bp.ensayo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableWebFlux
+@EnableScheduling
 @EnableR2dbcRepositories
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
--- src/main/java/com/bp/ensayo/routes/Routes.java
+++ src/main/java/com/bp/ensayo/routes/Routes.java
@@ -3,20 +3,9 @@ package com.bp.ensayo.routes;
 import com.bp.ensayo.handler.AccountHandler;
 import com.bp.ensayo.handler.CustomerHandler;
 import com.bp.ensayo.handler.TransactionHandler;
-import com.bp.ensayo.service.dto.AccountDTO;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -25,49 +14,7 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class Routes {
     @Bean
-    @RouterOperations({@RouterOperation(path = "/get/AccountDTOs", produces = {
-            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.GET, beanClass = AccountHandler.class, beanMethod = "getAllAccountDTO",
-
-            operation = @Operation(operationId = "getAllAccountDTO", responses = {
-                    @ApiResponse(responseCode = "200", description = "get all AccountDTO successfully.", content = @Content(schema = @Schema(implementation = AccountDTO.class)))})),
-            @RouterOperation(path = "/get/AccountDTOs/{AccountDTO_id}", produces = {
-                    MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.GET, beanClass = AccountHandler.class, beanMethod = "findById", operation = @Operation(operationId = "findById", responses = {
-                    @ApiResponse(responseCode = "200", description = "get AccountDTO successfully.", content = @Content(schema = @Schema(implementation = AccountDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "AccountDTO not found by given id.")}, parameters = {
-                    @Parameter(in = ParameterIn.PATH, name = "AccountDTO_id")})),
-
-            @RouterOperation(
-                    path = "/add/AccountDTO",
-                    produces = {
-                            MediaType.APPLICATION_JSON_VALUE
-                    },
-                    method = RequestMethod.POST,
-                    beanClass = AccountHandler.class,
-                    beanMethod = "addAccountDTO",
-                    operation = @Operation(
-                            operationId = "addAccountDTO",
-                            responses = {
-                                    @ApiResponse(
-                                            responseCode = "200",
-                                            description = "successful operation",
-                                            content = @Content(schema = @Schema(
-                                                    implementation = String.class
-                                            ))
-                                    )
-                            },
-                            requestBody = @RequestBody(
-                                    content = @Content(schema = @Schema(
-                                            implementation = AccountDTO.class
-                                    ))
-                            )
-
-                    )
-
-
-            )
-
-
-    })
+    @RouterOperations
     public RouterFunction<ServerResponse> accountsRoutes(AccountHandler handler) {
         return route().path("/accounts", builder -> builder
                 .GET("", handler::getAll)
