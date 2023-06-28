b123fd1db463b8fedd2f69efe434040fe8b46fc0
Bladimir Minga <bsminga@pichincha.com>
2023-06-27 22:15:57 -0500

    Se elimina cliente feing

--- src/main/java/com/bp/ensayo/App.java
+++ src/main/java/com/bp/ensayo/App.java
@@ -1,16 +1,11 @@
 package com.bp.ensayo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
-@EnableFeignClients
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
