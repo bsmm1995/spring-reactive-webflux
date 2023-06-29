d00d6c14a04fe9fa7cfdd67c6b475dd3b8469366
Bladimir Minga <bminga@pegasus.cl>
2023-06-28 22:37:17 -0500

    Fix api de hacer hacer retiro y trasnferencias

--- src/main/java/com/bp/ensayo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/bp/ensayo/service/impl/TransactionServiceImpl.java
@@ -3,7 +3,6 @@ package com.bp.ensayo.service.impl;
 import com.bp.ensayo.exception.AccountException;
 import com.bp.ensayo.repository.AccountRepository;
 import com.bp.ensayo.repository.TransactionRepository;
-import com.bp.ensayo.repository.entity.AccountEntity;
 import com.bp.ensayo.repository.entity.TransactionEntity;
 import com.bp.ensayo.repository.enums.AccountStatus;
 import com.bp.ensayo.repository.enums.TransactionType;
@@ -19,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.math.BigDecimal;
 import java.util.NoSuchElementException;
 
 @Service
@@ -44,22 +42,31 @@ public class TransactionServiceImpl implements TransactionService {
                     TransactionEntity credit = TransactionEntity.builder().transactionType(TransactionType.CREDIT).amount(data.getAmount()).accountId(account.getId()).build();
                     return transactionRepository.save(credit);
                 })
-                .doOnSuccess(e -> log.info("Se guardó la transacción."))
+                .doOnSuccess(e -> log.info("Se guardó la transacción de depósito."))
                 .map(transaction -> data);
     }
 
     @Override
     @Transactional
     public Mono<Transaction> makeWithdrawal(Transaction data) {
-        AccountEntity account = getAccountEntityByNumber(data.getAccountNumber());
-        if (account.getAmount().compareTo(data.getAmount()) < 0) {
-            throw new AccountException("Saldo insuficiente. Saldo actual " + account.getAmount());
-        }
-        account.setAmount(account.getAmount().subtract(data.getAmount()));
-        accountRepository.save(account);
-        log.info("Se guardó el retiro");
-        saveTransaction(account, data.getAmount(), TransactionType.DEBIT);
-        return Mono.just(data);
+        return accountRepository.findByAccountNumber(data.getAccountNumber())
+                .doOnNext(e -> log.info("Retiro for: {}", data))
+                .switchIfEmpty(Mono.error(new NoSuchElementException("No existe la cuenta número " + data.getAccountNumber())))
+                .doOnSuccess(account -> {
+                    if (account.getAmount().compareTo(data.getAmount()) < 0) {
+                        throw new AccountException("Saldo insuficiente. Saldo actual " + account.getAmount());
+                    }
+                })
+                .flatMap(account -> {
+                    account.setAmount(account.getAmount().subtract(data.getAmount()));
+                    return accountRepository.save(account);
+                }).doOnSuccess(e -> log.info("Se guardó el retiro."))
+                .flatMap(account -> {
+                    TransactionEntity credit = TransactionEntity.builder().transactionType(TransactionType.DEBIT).amount(data.getAmount()).accountId(account.getId()).build();
+                    return transactionRepository.save(credit);
+                })
+                .doOnSuccess(e -> log.info("Se guardó la transacción de retiro."))
+                .map(transaction -> data);
     }
 
     @Override
@@ -68,24 +75,41 @@ public class TransactionServiceImpl implements TransactionService {
         if (data.getAccountNumberOrigin().equals(data.getAccountNumberDestination())) {
             throw new AccountException("No se puede realizar transferencia entre la misma cuenta.");
         }
-        AccountEntity accountOrigen = getAccountEntityByNumber(data.getAccountNumberOrigin());
-        if (accountOrigen.getAmount().compareTo(data.getAmount()) < 0) {
-            throw new AccountException("Saldo insuficiente. Saldo actual " + accountOrigen.getAmount());
-        }
-
-        AccountEntity accountDestination = getAccountEntityByNumber(data.getAccountNumberDestination());
-        if (accountDestination.getStatus().equals(AccountStatus.INACTIVE)) {
-            throw new AccountException("La cuenta " + data.getAccountNumberDestination() + " no se encuentra activa.");
-        }
-        accountOrigen.setAmount(accountOrigen.getAmount().subtract(data.getAmount()));
-        accountRepository.save(accountOrigen);
-        log.info("Se guardó el debito en la cuenta origen");
-        accountDestination.setAmount(accountDestination.getAmount().add(data.getAmount()));
-        accountRepository.save(accountDestination);
-        log.info("Se guardó el credito en la cuenta destino");
-        saveTransaction(accountOrigen, data.getAmount(), TransactionType.DEBIT);
-        saveTransaction(accountDestination, data.getAmount(), TransactionType.CREDIT);
-        return Mono.just(data);
+        return accountRepository.findByAccountNumber(data.getAccountNumberOrigin())
+                .switchIfEmpty(Mono.error(new NoSuchElementException("No existe la cuenta número " + data.getAccountNumberOrigin())))
+                .doOnSuccess(accountOrigin -> {
+                    if (accountOrigin.getAmount().compareTo(data.getAmount()) < 0) {
+                        throw new AccountException("Saldo insuficiente. Saldo actual " + accountOrigin.getAmount());
+                    }
+                })
+                .flatMap(accountOrigin -> {
+                    accountOrigin.setAmount(accountOrigin.getAmount().subtract(data.getAmount()));
+                    return accountRepository.save(accountOrigin);
+                })
+                .flatMap(accountOrigin -> {
+                    TransactionEntity credit = TransactionEntity.builder().transactionType(TransactionType.DEBIT).amount(data.getAmount()).accountId(accountOrigin.getId()).build();
+                    return transactionRepository.save(credit);
+                })
+                .doOnSuccess(e -> log.info("Se guardó el debito a la cuenta."))
+                .flatMap(transaction ->
+                        accountRepository.findByAccountNumber(data.getAccountNumberDestination())
+                                .switchIfEmpty(Mono.error(new NoSuchElementException("No existe la cuenta número " + data.getAccountNumberDestination())))
+                                .doOnSuccess(accountDestination -> {
+                                    if (accountDestination.getStatus().equals(AccountStatus.INACTIVE)) {
+                                        throw new AccountException("La cuenta " + data.getAccountNumberDestination() + " no se encuentra activa.");
+                                    }
+                                })
+                                .flatMap(accountDestination -> {
+                                    accountDestination.setAmount(accountDestination.getAmount().add(data.getAmount()));
+                                    return accountRepository.save(accountDestination);
+                                })
+                                .flatMap(accountDestination -> {
+                                    TransactionEntity credit = TransactionEntity.builder().transactionType(TransactionType.CREDIT).amount(data.getAmount()).accountId(accountDestination.getId()).build();
+                                    return transactionRepository.save(credit);
+                                })
+                                .doOnSuccess(op -> log.info("Se guardó la acreditación a la cuenta."))
+                )
+                .map(transaction -> data);
     }
 
     @Override
@@ -94,15 +118,5 @@ public class TransactionServiceImpl implements TransactionService {
                 .doOnNext(e -> log.info("Report of: {}", e))
                 .switchIfEmpty(Mono.error(new NoSuchElementException("No existe la cuenta número " + accountNumber)))
                 .flatMapMany(e -> transactionRepository.findAllByAccountId(e.getId()).map(TransactionMapper.INSTANCE::toDto));
-    }
-
-    private AccountEntity getAccountEntityByNumber(String accountNumber) {
-        return accountRepository.findByAccountNumber(accountNumber).blockOptional().orElseThrow(() -> new NoSuchElementException("No existe la cuenta con numero " + accountNumber));
-    }
-
-    private void saveTransaction(AccountEntity account, BigDecimal amount, TransactionType type) {
-        TransactionEntity credit = TransactionEntity.builder().transactionType(type).amount(amount).accountId(account.getId()).build();
-        transactionRepository.save(credit);
-        log.info("Se guardó la transacción. cuenta={}, tipo={}", account.getAccountNumber(), type);
     }
 }
